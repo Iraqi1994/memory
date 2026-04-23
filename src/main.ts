ba3b172574867db16c623da3ce222daae108266a
Hans Müller <iraqihu1994@gmail.com>
2026-04-23 18:47:00 +0200

    Implemented 3d flipping animation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import "./styles/styles.scss";
 
-document.getElementById("h1_hallo")!.innerText = "Hallo Welt!";
+init();
+
+function init() {
+  const fieldRef = document.querySelector("#field");
+  if (fieldRef) {
+    fieldRef.addEventListener("click", (event) => {
+      const card = (event.target as HTMLElement).closest(".card") as HTMLButtonElement;
+      if (card) {
+        card.classList.toggle("is-flipped");
+      }
+    });
+  }
+}
